4f7acbca6f4e11fec9cce46ec4fc148f52a7ee3c
theory friction practice <jackson.brebnor@gmail.com>
2017-08-30 14:54:43 -0400

    authControls

--- src/authControls.js
+++ src/authControls.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { render, } from 'react-dom';
-import { StitchClient, } from 'mongodb-stitch';
-import { browserHistory, Route, } from 'react-router';
-import { BrowserRouter, Link, } from 'react-router-dom';
 
 import { db, items, stitchClient, users, } from './dbClient';
 import TodoItem from './todoItem';
